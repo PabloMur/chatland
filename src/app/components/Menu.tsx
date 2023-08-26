779833aa3159bbdbf61cb71a6506d56751052f69
PabloMur <polillomurillo@gmail.com>
2023-08-26 20:39:54 -0300

    muleta en menu desplegable

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -7,14 +7,17 @@ const Menu = () => {
   const active = useRecoilValue(menuActive);
   return (
     active && (
-      <div className="h-screen w-full sm:w-1/4 absolute top-0 left-0 bg-indigo-950 flex flex-col items-center justify-between p-4">
-        <ul className="h-3/5">
-          <MenuLi route="/home">Home</MenuLi>
-          <MenuLi route="/myRooms">My Rooms</MenuLi>
-          <MenuLi route="/profile">Profile</MenuLi>
-          <MenuLi route="/settings">Settings</MenuLi>
-        </ul>
-        <CloseSessionButton></CloseSessionButton>
+      <div>
+        <div className="h-screen w-full sm:w-1/4 absolute left-0 bg-indigo-950 flex flex-col items-center justify-between p-4">
+          <ul className="h-3/5">
+            <div className="h-16"></div>
+            <MenuLi route="/home">Home</MenuLi>
+            <MenuLi route="/myRooms">My Rooms</MenuLi>
+            <MenuLi route="/profile">Profile</MenuLi>
+            <MenuLi route="/settings">Settings</MenuLi>
+          </ul>
+          <CloseSessionButton></CloseSessionButton>
+        </div>
       </div>
     )
   );
