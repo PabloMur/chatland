7c34c25380f0236146c25fc62cc766b198b0ade6
PabloMur <polillomurillo@gmail.com>
2023-08-28 16:19:20 -0300

    fix: user/me

--- src/app/api/user/me/route.ts
+++ src/app/api/user/me/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import UserController from "@/lib/controllers/userController";
 
-export default async function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   try {
     const userData = await UserController.getUserData(request);
     return NextResponse.json({ userData }, { status: 200 });
