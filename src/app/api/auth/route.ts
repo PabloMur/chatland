1065d7d033b58dfdaacecd6aafa9bcd2dbed1bf4
PabloMur <polillomurillo@gmail.com>
2023-08-29 10:14:32 -0300

    cambios en controller

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import AuthController from "@/lib/controllers/authController";
+//import AuthController from "@/lib/controllers/authController";
 
 export async function POST(request: NextRequest) {
   //const auth = await AuthController.createAuth(request);
