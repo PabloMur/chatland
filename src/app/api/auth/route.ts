1aa47011cdcf7b7a93b4a19ccbe6bd0f55d899c0
PabloMur <polillomurillo@gmail.com>
2023-08-29 00:14:00 -0300

    agregue nextrequest

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import AuthController from "@/lib/controllers/authController";
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const auth = await AuthController.createAuth(request);
   if (auth) {
     return NextResponse.json({ authCreated: auth }, { status: 200 });
