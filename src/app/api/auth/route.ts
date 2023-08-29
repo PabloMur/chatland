fe5bf0966f005b91a2e92f95cae9782bd29516ee
PabloMur <polillomurillo@gmail.com>
2023-08-29 12:08:51 -0300

    casi sin api

--- src/app/api/auth/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import AuthController from "@/lib/controllers/authController";
-
-export async function POST(request: NextRequest) {
-  const auth = await AuthController.createAuth(request);
-  return NextResponse.json("test");
-}
