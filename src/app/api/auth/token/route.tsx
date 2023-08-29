8498d824680a29749d27433404ece5ffbbb30220
PabloMur <polillomurillo@gmail.com>
2023-08-28 23:17:39 -0300

    token

--- src/app/api/auth/token/route.tsx
+++ src/app/api/auth/token/route.tsx
@@ -3,7 +3,9 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   try {
     const token = await AuthController.retriveToken(request);
-    return NextResponse.json({ token }, { status: 200 });
+    if (token !== undefined && token !== null) {
+      return NextResponse.json({ token }, { status: 200 });
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error }, { status: 400 });
