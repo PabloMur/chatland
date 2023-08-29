fe0c937208c47b7796cbb3cb916bf15313627776
PabloMur <polillomurillo@gmail.com>
2023-08-28 23:19:46 -0300

    cambio de metodos

--- src/app/api/room/[roomId]/route.ts
+++ src/app/api/room/[roomId]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { firestoreDB } from "@/lib/FirestoreConn";
 //Endpoint para obtener la id de la chatroom mediante el codigo mas pequeño
 //Tarea: Hacer que esto funcione con un controller mejor
-export async function GET(request: NextRequest, { params }: any) {
+export async function POST(request: NextRequest, { params }: any) {
   try {
     const { roomId } = params;
     const snapshotRef = firestoreDB.collection("rooms").doc(roomId);
